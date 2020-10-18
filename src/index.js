21e2458c7af3fb63e40eb7aaf51c67d3a3ee570a
AlexeyBobkov <alekseixxxbobkov@bk.ru>
2020-10-18 21:50:27 +0300

    goal

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let splitCode = [];
+    let word = '';
+
+	splitCode.push(expr.slice(0, 10))
+    for(let i = 0; i < expr.length; i++) {
+        if((i + 1) % 10 === 0) {
+          splitCode.push(expr.slice((i - 9), (i + 1)))
+        }
+    } 
+   splitCode.splice(0, 1);
+   	
+    var morze = splitCode.map(function(numbers) {
+ 	numbers = numbers.replace(/00/g,'');
+    numbers = numbers.replace(/10/g,'.');
+    numbers = numbers.replace(/11/g,'-');
+    if (numbers === '**********') {
+  	    numbers = ' '
+     }  
+        return numbers
+    })
+    
+    for(let j = 0; j < morze.length; j++) {
+        word += (morze[j] === ' ') ? ' ' : MORSE_TABLE[morze[j]];
+    }
+    return word
 }
 
 module.exports = {
